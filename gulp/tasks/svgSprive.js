12a0d79f390b3d9f48062ebe5d71cb8804b69f5f
Anna Vasilevich <dzichonka@gmail.com>
2022-07-26 17:15:34 +0200

    svgSprite

---
+++ gulp/tasks/svgSprive.js
@@ -0,0 +1,19 @@
+import svgSprite from "gulp-svg-sprite";
+
+export const svgSprive = () => {
+    return app.gulp.src(`${app.path.src.svgicons}`, {})
+        .pipe(app.plugins.plumber(
+            app.plugins.notify.onError({
+                title: "SVG",
+                message: "Error: <%= error.mesage %>"
+            })))
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: `../icons/icons.svg`, //создавать страницу с перечнем иконок
+                    example: true
+                }
+            },
+        }))
+        .pipe(app.gulp.dest(`${app.path.build.images}`))
+}
